cc2c58c705dd717763e528faf02ad93ba7224196
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-27 21:47:15 -0300

    Adicionando novo teste de divisão por zero

--- src/test/java/bubbles/bubbles_actions/service/CalculadoraServiceTest.java
+++ src/test/java/bubbles/bubbles_actions/service/CalculadoraServiceTest.java
@@ -3,6 +3,8 @@ package bubbles.bubbles_actions.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class CalculadoraServiceTest {
     @Test
@@ -16,5 +18,21 @@ public class CalculadoraServiceTest {
         double resultado = calculadoraService.dividir(a, b);
 
         Assertions.assertEquals(esperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 0, deve lançar uma exceção")
+    public void testDividirPorZero() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 0;
+        var expectedMessage = "400 BAD_REQUEST \"Divisão por zero não permitida\"";
+
+        ResponseStatusException exception = Assertions.assertThrows(
+                ResponseStatusException.class, () -> {
+                    calculadoraService.dividir(a, b);
+                });
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 }
